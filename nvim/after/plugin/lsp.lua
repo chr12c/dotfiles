f9f4a897c69d5ae28505ec29d4d90d920da0fe73
chr12c <cchhristopher@gmail.com>
2023-02-25 17:04:16 +0000

    diagnostics virtual text

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -10,3 +10,8 @@ lsp.nvim_workspace()
 
 lsp.setup()
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+  vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    { virtual_text = true }
+  )
