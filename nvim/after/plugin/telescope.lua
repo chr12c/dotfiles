2672ff90bcaa2fc91b4204258768b61ca55b7a4d
chr12c <cchhristopher@gmail.com>
2023-02-25 19:32:26 +0000

    find_files looks for hidden files and disrepects .gitignore

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,3 +1,14 @@
+local telescope = require('telescope')
+
+telescope.setup {
+  pickers = {
+    find_files = {
+      hidden = true,
+      no_ignore = true
+    }
+  }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
