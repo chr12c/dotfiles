d8835d0f44e52e14bb283c39cff4125313c26358
chr12c <cchhristopher@gmail.com>
2023-02-28 01:28:35 +0000

    fs for telescope grep

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -12,7 +12,7 @@ telescope.setup {
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>g', function()
+vim.keymap.set('n', '<leader>fs', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
