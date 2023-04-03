d75659be5b4ad37c07cb881de8c43553aaa928a2
chr12c <cchhristopher@gmail.com>
2023-04-04 00:15:53 +0100

    harpoon

--- nvim/lua/core/packer.lua
+++ nvim/lua/core/packer.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
     -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use('ThePrimeagen/harpoon')
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use {
     'VonHeikemen/lsp-zero.nvim',
