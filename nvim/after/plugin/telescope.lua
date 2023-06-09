80f0a27a0ae6e73f559f3799dfe144e017adf1d7
chr12c <cchhristopher@gmail.com>
2023-06-09 22:06:56 +0100

    buffer picker shortcut

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -15,4 +15,4 @@ vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
