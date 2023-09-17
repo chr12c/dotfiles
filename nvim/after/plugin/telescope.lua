b406c3c63a90fbe9fc668c41b9e36232cb6846c3
chr12c <cchhristopher@gmail.com>
2023-09-17 14:07:59 +0100

    telescope marks shortcut

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -26,3 +26,4 @@ vim.keymap.set('n', '<leader>fs', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fm', builtin.marks, {})
