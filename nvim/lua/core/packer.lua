28ebfa7572aa1a4128470636f1d68f6018d645ca
chr12c <cchhristopher@gmail.com>
2024-01-28 12:16:14 +0000

    rose-pine

--- nvim/lua/core/packer.lua
+++ nvim/lua/core/packer.lua
@@ -49,4 +49,11 @@ return require('packer').startup(function(use)
       "mfussenegger/nvim-dap",
     },
   }
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+    end
+  })
 end)
