066f85caf2a54596bbd0811e0fc75edf8bcb5752
chr12c <cchhristopher@gmail.com>
2024-05-18 16:28:04 +0100

    luasnip and cmp

--- nvim/lua/core/lazy/lsp.lua
+++ nvim/lua/core/lazy/lsp.lua
@@ -8,8 +8,12 @@ return {
     "hrsh7th/cmp-path",
     "hrsh7th/nvim-cmp",
     "hrsh7th/cmp-cmdline",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
   },
   config = function()
+    local cmp = require("cmp")
+
     require("mason").setup({})
     require("mason-lspconfig").setup({
       ensure_installs = {
@@ -20,6 +24,36 @@ return {
           require("lspconfig")[server_name].setup{}
         end
       }
+    })
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+        end
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_prev_item({behavior = "select"})
+          else
+            cmp.complete()
+          end
+        end),
+        ["<C-n>"] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_next_item({behavior = "select"})
+          else
+            cmp.complete()
+          end
+        end)
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- For luasnip users.
+      }, {
+        { name = "buffer" },
+      })
     })
   end
 }
