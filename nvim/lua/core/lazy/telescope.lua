0b683ef5c6bedf3d380f84e1a0e885cb6f33f33d
chr12c <cchhristopher@gmail.com>
2024-05-18 15:55:15 +0100

    web devicons

--- nvim/lua/core/lazy/telescope.lua
+++ nvim/lua/core/lazy/telescope.lua
@@ -1,7 +1,8 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons"
   },
 
   config = function()
