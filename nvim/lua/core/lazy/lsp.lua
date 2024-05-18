bb18c395322117d90abded2848124f1d78393a39
chr12c <cchhristopher@gmail.com>
2024-05-18 15:51:51 +0100

    lsp wip

---
+++ nvim/lua/core/lazy/lsp.lua
@@ -0,0 +1,25 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-cmdline",
+  },
+  config = function()
+    require("mason").setup({})
+    require("mason-lspconfig").setup({
+      ensure_installs = {
+        "lua_ls"
+      },
+      -- handlers = {
+      --   function(server_name)
+      --     require("lspconfig")[server_name].setup{}
+      --   end
+      -- }
+    })
+  end
+}
