f903a5acce57447fe3cbd7ce8e8da8907a202b06
chr12c <cchhristopher@gmail.com>
2024-05-18 14:02:49 +0100

    harpoon

--- nvim/lua/core/lazy/telescope-file-browser.lua
+++ nvim/lua/core/lazy/telescope-file-browser.lua
@@ -1,26 +1,26 @@
 return {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim"
-    },
+  "nvim-telescope/telescope-file-browser.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim"
+  },
 
-    config = function()
-      require("telescope").load_extension "file_browser"
+  config = function()
+    require("telescope").load_extension "file_browser"
 
-      vim.api.nvim_set_keymap(
-        "n",
-        "<leader>fp",
-        ":Telescope file_browser<CR>",
-        { noremap = true }
-      )
+    vim.api.nvim_set_keymap(
+    "n",
+    "<leader>fp",
+    ":Telescope file_browser<CR>",
+    { noremap = true }
+    )
 
-      -- open file_browser with the path of the current buffer
-      vim.api.nvim_set_keymap(
-        "n",
-        "<leader>fc",
-        ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-        { noremap = true }
-      )
-    end
+    -- open file_browser with the path of the current buffer
+    vim.api.nvim_set_keymap(
+    "n",
+    "<leader>fc",
+    ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+    { noremap = true }
+    )
+  end
 }
--- nvim/lua/core/lazy_init.lua
+++ nvim/lua/core/lazy_init.lua
@@ -12,6 +12,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "core.lazy",
-    change_detection = { notify = false }
+  spec = "core.lazy",
+  change_detection = { notify = false }
 })
