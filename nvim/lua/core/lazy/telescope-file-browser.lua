714ef4b4890a94916a00b24948a30f959d3bb8b2
chr12c <chris.chiang@sky.uk>
2024-05-19 11:13:06 +0100

    fmt

--- nvim/lua/core/lazy/telescope-file-browser.lua
+++ nvim/lua/core/lazy/telescope-file-browser.lua
@@ -9,18 +9,18 @@ return {
     require("telescope").load_extension "file_browser"
 
     vim.api.nvim_set_keymap(
-    "n",
-    "<leader>fp",
-    ":Telescope file_browser<CR>",
-    { noremap = true }
+      "n",
+      "<leader>fp",
+      ":Telescope file_browser<CR>",
+      { noremap = true }
     )
 
     -- open file_browser with the path of the current buffer
     vim.api.nvim_set_keymap(
-    "n",
-    "<leader>fc",
-    ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-    { noremap = true }
+      "n",
+      "<leader>fc",
+      ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+      { noremap = true }
     )
   end
 }
