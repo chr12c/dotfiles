9a9297aebf24dd5f2deb989aed539c14225f37ca
chr12c <cchhristopher@gmail.com>
2025-03-17 00:01:17 +0000

    Toggle vim fugitive when buffer is open or not

--- nvim/lua/core/lazy/vim-fugitive.lua
+++ nvim/lua/core/lazy/vim-fugitive.lua
@@ -1,3 +1,13 @@
 return {
-  "tpope/vim-fugitive"
+  'tpope/vim-fugitive',
+  config = function()
+    vim.keymap.set('n', '<leader>ee', function()
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        if vim.bo[vim.api.nvim_win_get_buf(win)].filetype == 'fugitive' then
+          return vim.api.nvim_win_close(win, false) -- Close if open
+        end
+      end
+      vim.cmd.Git() -- Open if not open
+    end, { desc = 'Toggle Git status' })
+  end
 }
