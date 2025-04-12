8bf1847edcd36981991a89cf0dea653ac8b787a0
chr12c <cchhristopher@gmail.com>
2025-04-12 15:36:36 +0100

    Defer to nvim 0.11.0 defaults and consequently remove the following lsp custom shortcuts: implementation, references, doc symbol, rename, code action, hover, prev diagnostic, next diagnostic

--- nvim/lua/core/lsp_maps.lua
+++ nvim/lua/core/lsp_maps.lua
@@ -1,16 +1,10 @@
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration)
-vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation)
 vim.keymap.set("n", "<leader>go", vim.lsp.buf.type_definition)
-vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references)
 vim.keymap.set("n", "<leader>gs", vim.lsp.buf.signature_help)
-vim.keymap.set("n", "<leader>gys", vim.lsp.buf.document_symbol)
 vim.keymap.set("n", "<leader>gws", vim.lsp.buf.workspace_symbol)
 vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run)
-vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
 vim.keymap.set("n", "<F3>", vim.lsp.buf.format)
-vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action)
 
 vim.keymap.set("n", "<leader>gl", vim.diagnostic.open_float)
 
@@ -29,11 +23,3 @@ end)
 
 -- buffer diagnostics only
 vim.keymap.set("n", "<leader>db", vim.diagnostic.setloclist)
-
-vim.keymap.set("n", "<leader>[d", function()
-  vim.diagnostic.goto_prev({ wrap = false })
-end)
-
-vim.keymap.set("n", "<leader>]d", function()
-  vim.diagnostic.goto_next({ wrap = false })
-end)
