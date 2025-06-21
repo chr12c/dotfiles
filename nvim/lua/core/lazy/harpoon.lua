4cc0715e7dcc4fe26f0f3ee9cfd79460f4acbead
chr12c <cchhristopher@gmail.com>
2025-06-21 16:17:03 +0100

    Move simple harpoon bindings to keys field

--- nvim/lua/core/lazy/harpoon.lua
+++ nvim/lua/core/lazy/harpoon.lua
@@ -2,18 +2,20 @@ return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
+  keys = function()
+    local harpoon = require("harpoon")
+    return {
+      { "<leader>ha", function() harpoon:list():add() end },
+      { "<leader>ht", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end },
+      { "<leader>h1", function() harpoon:list():select(1) end },
+      { "<leader>h2", function() harpoon:list():select(2) end },
+      { "<leader>h3", function() harpoon:list():select(3) end },
+      { "<leader>h4", function() harpoon:list():select(4) end }
+    }
+  end,
   config = function()
     local harpoon = require("harpoon")
-
     harpoon.setup()
-
-    vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<leader>ht", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-    vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
 
     harpoon:extend({
       UI_CREATE = function(cx)
