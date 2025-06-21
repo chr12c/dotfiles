97dd6df3ed2116adb4a7dfcae0a6dd1031ef21c7
chr12c <cchhristopher@gmail.com>
2025-06-21 16:03:45 +0100

    Move simple gitsigns keymappings into the keys field

--- nvim/lua/core/lazy/gitsigns.lua
+++ nvim/lua/core/lazy/gitsigns.lua
@@ -1,5 +1,35 @@
 return {
   "lewis6991/gitsigns.nvim",
+  keys = function()
+    local gitsigns = require("gitsigns")
+    return {
+      { "<leader>is", gitsigns.stage_hunk, desc = "Git stage hunk" },
+      { "<leader>ir", gitsigns.reset_hunk, desc = "Git reset hunk" },
+      {
+        "<leader>is",
+        mode = "v",
+        function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v"), }) end,
+        desc = "Git stage highlighted hunk"
+      },
+      {
+        "<leader>ir",
+        mode = "v",
+        function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v"), }) end,
+        desc = "Git reset highlighted hunk"
+      },
+      { "<leader>iu", gitsigns.undo_stage_hunk, desc = "Git undo stage hunk" },
+      { "<leader>iS", gitsigns.stage_buffer,    desc = "Git stage buffer" },
+      { "<leader>iR", gitsigns.reset_buffer,    desc = "Git reset buffer" },
+      { "<leader>ip", gitsigns.preview_hunk,    desc = "Git preview hunk" },
+      {
+        "<leader>ib",
+        function() gitsigns.blame_line({ full = true }) end,
+        desc = "Git blame line"
+      },
+      { "<leader>iB", gitsigns.toggle_current_line_blame, desc = "Git toggle current line blame" },
+      { "<leader>it", gitsigns.toggle_deleted,            desc = "Git toggle deleted" },
+    }
+  end,
   config = function()
     require("gitsigns").setup({
       on_attach = function(bufnr)
@@ -11,10 +41,9 @@ return {
           vim.keymap.set(mode, l, r, opts)
         end
 
-        -- Navigation
         map("n", "<leader>i]", function()
           if vim.wo.diff then
-            vim.cmd.normal({"<leader>i]", bang = true})
+            vim.cmd.normal({ "<leader>i]", bang = true })
           else
             gitsigns.nav_hunk("next")
           end
@@ -22,28 +51,12 @@ return {
 
         map("n", "<leader>i[", function()
           if vim.wo.diff then
-            vim.cmd.normal({"<leader>i[", bang = true})
+            vim.cmd.normal({ "<leader>i[", bang = true })
           else
             gitsigns.nav_hunk("prev")
           end
         end)
-
-        -- Actions
-        map("n", '<leader>is', gitsigns.stage_hunk)
-        map("n", "<leader>ir", gitsigns.reset_hunk)
-        map("v", "<leader>is", function() gitsigns.stage_hunk {vim.fn.line("."), vim.fn.line("v")} end)
-        map("v", "<leader>ir", function() gitsigns.reset_hunk {vim.fn.line("."), vim.fn.line("v")} end)
-        map("n", "<leader>iS", gitsigns.stage_buffer)
-        map("n", "<leader>iu", gitsigns.undo_stage_hunk)
-        map("n", "<leader>iR", gitsigns.reset_buffer)
-        map("n", "<leader>ip", gitsigns.preview_hunk)
-        map("n", '<leader>ib', function() gitsigns.blame_line{full=true} end)
-        map("n", '<leader>iB', gitsigns.toggle_current_line_blame)
-        -- map("n", "<leader>id", gitsigns.diffthis)
-        -- map("n", '<leader>iD', function() gitsigns.diffthis('~') end)
-        map("n", "<leader>it", gitsigns.toggle_deleted)
       end
-
     })
   end
 }
