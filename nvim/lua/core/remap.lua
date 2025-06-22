5a3c25c32f60e2ab3fce7455807c92d776a1b7ac
chr12c <cchhristopher@gmail.com>
2025-06-22 21:43:39 +0100

    keymaps: add tabclose and tabonly

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -1,2 +1,5 @@
-vim.keymap.set('n', '<Space>', '<Nop>', { silent = true })
-vim.g.mapleader = ' '
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { desc = "Close tab", silent = true })
+vim.keymap.set("n", "<leader>to", ":tabonly<CR>", { desc = "Close all other tabs", silent = true })
