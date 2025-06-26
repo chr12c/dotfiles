3bd4c051bd69706441a0b4d38afd84022e36d8a0
chr12c <cchhristopher@gmail.com>
2025-06-26 14:48:07 +0100

    add keymaps for resizing neovim windows

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -3,3 +3,8 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { desc = "Close tab", silent = true })
 vim.keymap.set("n", "<leader>to", ":tabonly<CR>", { desc = "Close all other tabs", silent = true })
+
+vim.keymap.set("n", "<M-->", "<C-w>-", { desc = "Decrease current window height" })
+vim.keymap.set("n", "<M-=>", "<C-w>+", { desc = "Increase current window height" })
+vim.keymap.set("n", "<M-,>", "<C-w><", { desc = "Decrease current window width" })
+vim.keymap.set("n", "<M-.>", "<C-w>>", { desc = "Increase current window width" })
