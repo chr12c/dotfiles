c1471065ac3bcb3770ecd4223be69f5a3001bb96
chr12c <cchhristopher@gmail.com>
2025-07-27 23:05:18 +0100

    nvim(gitsigns): move lazy.nvim style keymapping declarations to config section

--- nvim/lua/core/lazy/gitsigns.lua
+++ nvim/lua/core/lazy/gitsigns.lua
@@ -1,35 +1,5 @@
 return {
   "lewis6991/gitsigns.nvim",
-  keys = function()
-    local gitsigns = require("gitsigns")
-    return {
-      { "<leader>is", gitsigns.stage_hunk, desc = "Git stage hunk" },
-      { "<leader>ir", gitsigns.reset_hunk, desc = "Git reset hunk" },
-      {
-        "<leader>is",
-        mode = "v",
-        function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v"), }) end,
-        desc = "Git stage highlighted hunk"
-      },
-      {
-        "<leader>ir",
-        mode = "v",
-        function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v"), }) end,
-        desc = "Git reset highlighted hunk"
-      },
-      { "<leader>iS", gitsigns.stage_buffer, desc = "Git stage buffer" },
-      { "<leader>iR", gitsigns.reset_buffer, desc = "Git reset buffer" },
-      { "<leader>ip", gitsigns.preview_hunk, desc = "Git preview hunk" },
-      { "<leader>ib", gitsigns.blame,        desc = "Git blame" },
-      {
-        "<leader>iB",
-        function() gitsigns.blame_line({ full = true }) end,
-        desc = "Git blame line"
-      },
-      { "<leader>id", function() gitsigns.diffthis("@") end,      desc = "Git diff against HEAD" },
-      { "<leader>iD", function() gitsigns.diffthis("master") end, desc = "Git diff against master" }
-    }
-  end,
   config = function()
     require("gitsigns").setup({
       on_attach = function(bufnr)
@@ -56,6 +26,24 @@ return {
             gitsigns.nav_hunk("prev")
           end
         end)
+
+        map("n", "<leader>is", gitsigns.stage_hunk, { desc = "Git stage hunk" })
+        map("n", "<leader>ir", gitsigns.reset_hunk, { desc = "Git reset hunk" })
+        map("v", "<leader>is",
+          function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v"), }) end,
+          { desc = "Git stage highlighted hunk" }
+        )
+        map("v", "<leader>ir",
+          function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v"), }) end,
+          { desc = "Git reset highlighted hunk" }
+        )
+        map("n", "<leader>iS", gitsigns.stage_buffer, { desc = "Git stage buffer" })
+        map("n", "<leader>iR", gitsigns.reset_buffer, { desc = "Git reset buffer" })
+        map("n", "<leader>ip", gitsigns.preview_hunk, { desc = "Git preview hunk" })
+        map("n", "<leader>ib", gitsigns.blame, { desc = "Git blame" })
+        map("n", "<leader>iB", function() gitsigns.blame_line({ full = true }) end, { desc = "Git blame line" })
+        map("n", "<leader>id", function() gitsigns.diffthis("@") end, { desc = "Git diff against HEAD" })
+        map("n", "<leader>iD", function() gitsigns.diffthis("master") end, { desc = "Git diff against master" })
       end
     })
   end
