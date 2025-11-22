5a2b9ea9485069ca82a93fb82163d3de36623475
chr12c <cchhristopher@gmail.com>
2025-11-22 22:42:07 +0000

    nvim(lsp maps): rely on default key binding for diagnostic float, repurpose declaration binding for goto def in vsplit

--- nvim/lua/core/lsp_maps.lua
+++ nvim/lua/core/lsp_maps.lua
@@ -1,12 +1,13 @@
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "<leader>gD", function()
+  vim.cmd.vsplit()
+  vim.lsp.buf.definition()
+end, { desc = "Go to definition (vsplit)" })
 vim.keymap.set("n", "<leader>go", vim.lsp.buf.type_definition)
 vim.keymap.set("n", "<leader>gs", vim.lsp.buf.signature_help)
 vim.keymap.set("n", "<leader>gws", vim.lsp.buf.workspace_symbol)
 vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run)
 vim.keymap.set("n", "<F3>", vim.lsp.buf.format)
-
-vim.keymap.set("n", "<leader>gl", vim.diagnostic.open_float)
 
 -- all workspace diagnostics
 vim.keymap.set("n", "<leader>aa", vim.diagnostic.setqflist)
